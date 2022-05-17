38742228cb07a352cb1eda48155da7cbc8ef9bb0
GeneralOlutoye <donronald27@gmail.com>
2022-05-17 10:27:41 +0100

    Updated Home-Practice

--- Bro code/script.js
+++ Bro code/script.js
@@ -14,10 +14,24 @@
 //     document.getElementById('myLabel').innerHTML= 'hello ' + username
 // }
 
-let myText= document.querySelector('#myText')
-let user
+// let myText= document.querySelector('#myText')
+// let user
 
-document.querySelector('#myButton').addEventListener('click', ()=>{
-    user= myText.value
-    document.querySelector('#myLabel').innerHTML= user
-}) 
+// document.querySelector('#myButton').addEventListener('click', ()=>{
+//     user= myText.value
+//     document.querySelector('#myLabel').innerHTML= user
+// }) 
+
+let count = 0;
+ document.querySelector('#decreaseBtn').onclick= ()=>{
+     count-=1
+     document.getElementById('countLabel').innerHTML= count
+ }
+ document.querySelector('#reset').onclick= ()=>{
+     count=0
+     document.getElementById('countLabel').innerHTML= count
+ }
+ document.querySelector('#increaseBtn').onclick= ()=>{
+     count+=1
+     document.getElementById('countLabel').innerHTML= count
+ }
--- JS/JSreview.js
+++
@@ -1,5 +0,0 @@
-//code to print out date and time
-var currentDate= new Date
-console.log("current date and time is " + currentDate)
-
-var ikorodu
