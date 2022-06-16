373f401f6748d7e543ba009d065efa486ad21846
General Olutoye <donronald27@gmail.com>
2022-06-16 10:49:48 +0100

    Updated

--- Bro-code/counter/script.js
+++ Bro-code/counter/script.js
@@ -1,38 +1,42 @@
-// let username
-
-// document.getElementById('myButton').onclick = function(){
-
-//     username= document.getElementById('myText').value
-//     console.log(username)
-//     document.getElementById('myLabel').innerHTML= 'Hello ' + username
-
-// }
-
-// document.getElementById('myButton').onclick= ()=>{
-//     username= document.getElementById('myText').value
-//     console.log(username)
-//     document.getElementById('myLabel').innerHTML= 'hello ' + username
-// }
-
-// let myText= document.querySelector('#myText')
-// let user
-
-// document.querySelector('#myButton').addEventListener('click', ()=>{
-//     user= myText.value
-//     document.querySelector('#myLabel').innerHTML= user
-// }) 
-
-let count = 0;
+let count= 0
+let counts = ""
  document.querySelector('#decreaseBtn').onclick= ()=>{
     if(count<=0){document.getElementById(`countLabel `).innerHTML=Display(0)}
-     count-=1
+     count-=100
      document.getElementById('countLabel').innerHTML= count
  }
+
  document.querySelector('#reset').onclick= ()=>{
      count=0
      document.getElementById('countLabel').innerHTML= count
  }
- document.querySelector('#increaseBtn').onclick= ()=>{
-     count+=1
+
+//  document.querySelector('#increaseBtn').onclick= ()=>{
+//      count+=100
+
+//      document.getElementById('countLabel').innerHTML= count
+//  }
+
+
+
+clicked= ()=>{
+    count+=100
      document.getElementById('countLabel').innerHTML= count
- }
+     document.getElementById('output').innerHTML = counts;
+     counts+= "count is " + i + "<br>";
+}
+
+document.getElementById('increaseBtn').addEventListener('click', clicked) =  document.getElementById('output').innerHTML = counts;
+document.getElementById('output').innerHTML = counts;
+
+// for(let i=0; i<=1000; i+=100){
+//     counts+= "count is " + i + "<br>";
+// }
+
+// clicks= ()=>{
+    
+//     if(count<=0){document.getElementById(`countLabel `).innerHTML=Display(0)}
+//      count-=100
+// }
+// document.getElementById('decreaseBtn').addEventListener('click', clicked)
+// document.getElementById('output').innerHTML = counts;
